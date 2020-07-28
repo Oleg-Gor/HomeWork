2d6be570cd469a15d914fa02b8eb12cfd70ac353
Oleg <vsestilno.com.ua>
2020-07-28 17:10:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+//a=5;
+//console.log(a);
+
+var number = 5;
+var string = "hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log ('string'*9);
+
+let something;
+console.log(something);
+
+let person = {
+name: "Jone",
+age: 25,
+isMarried: false
+
+};
+
+/*console.log(person["age"]);
+
+let arr = ['plum.png','orange.jph','apple.bmp'];
+
+console.log(arr[2]);*/
+
+//alert("hello world!");
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+/*
+let answer = +prompt("are you 18?", "да");
+
+console.log(typeof(answer));
+
+console.log("art"+"- object");  
+console.log(4+"-object");
+*/
+let incr = 10,
+    decr = 10;
+
+   
+
+    console.log(incr++);
+    console.log(decr--);
+    
