462e22ddca23a7cd9a9be85ac52767f66141eeca
Oleg <vsestilno.com.ua>
2020-07-29 16:27:08 +0300

    Worked with new file

---
+++ js.js
@@ -0,0 +1,31 @@
+let num = 50;
+
+if (num < 49) {
+    console.log("Neverno")
+} else if (num > 100) {
+
+    console.log ("mnogo")
+} else { 
+    console.log ('Verno')
+} console.log (num);
+
+(num == 30) ? console.log('Verno!!') : console.log('Neverno!!!');
+
+switch (num) {
+case num < 49:
+    console.log ('neverno 3');
+    break;
+case num > 100:
+    console.log ('mnogo 3');
+    break;
+case num > 80:
+    console.log ('vse eche mnogo 3');
+    break;     
+case 50:
+    console.log('Verno 3');
+    break;
+default:
+    console.log ("chto-to poshlo ne tak 3");
+    break;
+}
+
